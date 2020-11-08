79c16635a16870471cf715e04dbe6fc38c9c1e9b
Ali Benkassou <a.benkassou1@gmail.com>
2020-11-08 23:20:41 +0100

    Add utility script to download data from remote influx db

---
+++ utils/download.sh
@@ -0,0 +1,27 @@
+#!/bin/bash
+# Usage: ./download.sh [ip] [port]
+
+
+# useful variables
+host=$1
+port=$2
+data_dir=data_$(echo "$host" | cut -d':' -f 1)
+
+# create output directory
+if [ -d "$data_dir" ]; then
+    rm -r $data_dir
+fi
+mkdir $data_dir
+
+# get list of measurements
+measurements=$(curl -s -G http://"$host":"$port"/query --data-urlencode "q=SHOW measurements ON monitoring" -H "Accept: application/csv" | cut -d',' -f3 | tail -n +2)
+
+# download and store in separate csv files
+for var in $measurements;
+do
+    echo -e Downloading "\033[1m"$var"\033[0m" ...
+    docker run --rm influxdb influx -host $host -port $port -database 'monitoring' -execute 'SELECT * from '"$var" -format csv > $data_dir/$var.csv
+done
+
+echo Summary:
+du $data_dir/* -ch
