5579696f9879dde9b9348e9c69b6c26dcc20b8ff
Ali Benkassou <a.benkassou1@gmail.com>
2020-12-18 00:56:36 +0100

    Add Time2Vec module

--- batteryprobe/models.py
+++ batteryprobe/models.py
@@ -2,82 +2,13 @@
 
 import torch
 from torch import nn
-from torch.nn.utils.rnn import pad_packed_sequence, pack_padded_sequence
+from torch.nn import init
+from torch.nn.utils.rnn import pad_packed_sequence, pack_padded_sequence, pad_sequence
 
 from batteryprobe.utils import pad_and_pack
 
 
 # pylint: disable=C0103
-class AutoRegressive(nn.Module):
-    """AutoRegressive model.
-
-    Attributes:
-        in_size (int): Size in input of the model.
-        out_size (init): Size in output of the model.
-        lstm (nn.LSTM): The lstm layer.
-        dense (nn.Linear): The dense layer.
-
-    Args:
-        params(dict): Parameters dict.
-    """
-
-    def __init__(self, params):
-        self.params = params
-        super().__init__()
-        self.in_size = len(self.params["features"]) + len(self.params["context"])
-        self.out_size = len(self.params["features"])
-        self.lstm = nn.LSTM(self.in_size, 64, num_layers=params["lstm_layers"], batch_first=True)
-        self.dense = nn.Linear(64, self.out_size)
-
-    # pylint: disable=W0613
-    def forward(self, x, time, context):
-        """Forward pass.
-
-        Args:
-            x (torch.Tensor): inputs
-            time (torch.nn.utils.rnn.PackedSequence): Timestamps
-            context (torch.nn.utils.rnn.PackedSequence): Context
-        """
-        x, warmup_state = self._warmup(x)
-
-        context, lengths_context = pad_packed_sequence(
-            context,
-            batch_first=True,
-            padding_value=-999
-        )
-        # Loop over every element of a batch
-        batch = []
-        for i, element in enumerate(x):
-            # State corresponding to a single element in a batch
-            state = (  # (num_layers, batch, hidden_size)
-                torch.unsqueeze(warmup_state[0][:, i, :], 1),
-                torch.unsqueeze(warmup_state[1][:, i, :], 1),
-            )
-
-            # Add first prediction from warmup
-            timestamps = [element[None, None, :]]
-
-            # Predict values
-            # pylint: disable=C0103
-            for t in range(lengths_context[i] - 1):
-                # The order here is important. Inputs features should be first
-                in_tensor = torch.cat([element, context[i, t, :]], axis=-1)[None, None, :]
-                x, state = self.lstm(in_tensor, state)
-                x = self.dense(x)
-                timestamps.append(x)
-            batch.append(torch.cat(timestamps, 1)[0])
-
-        # Pack and pad sequence
-        return pad_and_pack(batch)
-
-    def _warmup(self, x):
-        x, state = self.lstm(x)
-        x, lengths = pad_packed_sequence(x, batch_first=True, padding_value=-999)
-        x = torch.stack([x[i, length - 1] for i, length in enumerate(lengths)])
-        x = self.dense(x)
-        return x, state
-
-
 class Baseline(nn.Module):
     """Baseline model.
 
@@ -117,3 +48,130 @@ class Baseline(nn.Module):
         res = pack_padded_sequence(res,
             len_context, batch_first=True, enforce_sorted=False)
         return res
+
+
+class Time2Vec(nn.Module):
+    """Time2Vec module.
+
+    Implements: https://arxiv.org/abs/1907.05321
+    """
+    def __init__(self, k, periodic_activation_function=torch.sin):
+        super().__init__()
+        self.k = k
+        self.periodic_activation_fn = periodic_activation_function
+        omega = torch.Tensor(k)
+        self.omega = nn.Parameter(omega)
+        phi = torch.Tensor(k)
+        self.phi = nn.Parameter(phi)
+
+        # Initialization phase and frequency
+        bound = 1
+        init.uniform_(self.omega, -bound, bound)
+        init.uniform_(self.phi, -bound, bound)
+
+    def forward(self, t):
+        """Forward pass.
+
+        Args:
+            t (torch.Tensor): (batch_size, sequence_len, 1)
+
+        Returns:
+            (torch.Tensor): (batch_size, sequence_len, k)
+        """
+        return self.periodic_activation_fn(t @ self.omega[None, :] + self.phi)
+
+
+class AutoRegressive(nn.Module):
+    """AutoRegressive model.
+
+    Attributes:
+        in_size (int): Size in input of the model.
+        out_size (init): Size in output of the model.
+        lstm (nn.LSTM): The lstm layer.
+        dense (nn.Linear): The dense layer.
+
+    Args:
+        params(dict): Parameters dict.
+    """
+
+    def __init__(self, params):
+        self.params = params
+        super().__init__()
+        self.in_size = len(self.params["features"]) + len(self.params["context"]) + params["t2v_k"]
+        self.out_size = len(self.params["features"])
+        self.t2v = Time2Vec(k=params["t2v_k"])
+        self.lstm = nn.LSTM(self.in_size, 64, num_layers=params["lstm_layers"], batch_first=True)
+        self.dense = nn.Linear(64, self.out_size)
+
+    # pylint: disable=W0613
+    def forward(self, x, time, context):
+        """Forward pass.
+
+        Args:
+            x (torch.Tensor): inputs
+            time (torch.nn.utils.rnn.PackedSequence): Timestamps
+            context (torch.nn.utils.rnn.PackedSequence): Context
+        """
+        # Embed time and add it to inputs
+        x, time_embedding = self.embed_time(x, time)
+
+        # Pass into warmup
+        x, warmup_state = self._warmup(x)
+
+        # Unpack context for outputs prediction
+        context, lengths_context = pad_packed_sequence(
+            context,
+            batch_first=True,
+            padding_value=-999
+        )
+        # Loop over every element of a batch
+        batch = []
+        for i, element in enumerate(x):
+            # State corresponding to a single element in a batch
+            state = (  # (num_layers, batch, hidden_size)
+                torch.unsqueeze(warmup_state[0][:, i, :], 1),
+                torch.unsqueeze(warmup_state[1][:, i, :], 1),
+            )
+
+            # Add first prediction from warmup
+            timestamps = [element[None, None, :]]
+
+            # Predict values
+            # pylint: disable=C0103
+            for t in range(lengths_context[i] - 1):
+                # The order here is important. Inputs features should be first
+                in_tensor = torch.cat(
+                    [element, context[i, t, :], time_embedding[i, t, :]],
+                    axis=-1
+                )[None, None, :]
+                x, state = self.lstm(in_tensor, state)
+                x = self.dense(x)
+                timestamps.append(x)
+            batch.append(torch.cat(timestamps, 1)[0])
+
+        # Pack and pad sequence
+        return pad_and_pack(batch)
+
+    def _warmup(self, x):
+        x, state = self.lstm(x)
+        x, lengths = pad_packed_sequence(x, batch_first=True, padding_value=-999)
+        x = torch.stack([x[i, length - 1] for i, length in enumerate(lengths)])
+        x = self.dense(x)
+        return x, state
+
+    def embed_time(self, x, time):
+        """Use Time2Vec layer to compute a time embedding and add it to input features."""
+        time, _ = pad_packed_sequence(time,
+            batch_first=True, padding_value=-999
+        )
+        time_embedding = self.t2v(torch.unsqueeze(time, -1))
+        x, x_len = pad_packed_sequence(x,
+            batch_first=True, padding_value=-999
+        )
+        inputs_time_embedding = pad_sequence([
+            time_embedding[i, :length] for i, length in enumerate(x_len)],
+            batch_first=True, padding_value=-999
+        )
+        x = torch.cat((x, inputs_time_embedding), 2)
+        x = pack_padded_sequence(x, x_len, batch_first=True, enforce_sorted=False)
+        return x, time_embedding
